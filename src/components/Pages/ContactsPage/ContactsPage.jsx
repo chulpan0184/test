69f70b839b01fa5bc63c627ff8ef32780424dc17
chulpan0184 <Chulpanbelova0184@gmail.com>
2023-02-19 13:39:31 +0300

    add tel in ContactPage

--- src/components/Pages/ContactsPage/ContactsPage.jsx
+++ src/components/Pages/ContactsPage/ContactsPage.jsx
@@ -1,6 +1,9 @@
 /* eslint-disable linebreak-style */
 export function ContactsPage() {
   return (
-    <p>ContactsPage</p>
+    <>
+      <p>ContactsPage</p>
+      <h1>tel 9777979797979</h1>
+    </>
   )
 }
